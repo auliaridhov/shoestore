95c93668c736887f9aa6918de42649a266f2eb00
auliaridhov <auliaridhoverdika@gmail.com>
2024-12-11 16:39:33 +0700

    fix thumbnail

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -102,7 +102,7 @@
                     <a href="{{route('front.details', $itemNewShoe->slug)}}">
                         <div class="flex items-center rounded-3xl p-[10px_16px_16px_10px] gap-[14px] bg-white transition-all duration-300 hover:ring-2 hover:ring-[#FFC700]">
                             <div class="w-20 h-20 flex shrink-0 rounded-2xl bg-[#D9D9D9] overflow-hidden">
-                                <img src="{{Storage::url($itemPopularShoe->thumbnail)}}" class="w-full h-full object-cover" alt="thumbnail">
+                                <img src="{{Storage::url($itemNewShoe->thumbnail)}}" class="w-full h-full object-cover" alt="thumbnail">
                             </div>
                             <div class="flex w-full items-center justify-between gap-[14px]">
                                 <div class="flex flex-col gap-[6px]">
@@ -151,7 +151,6 @@
                 </nav>
             </div>
         </div>
-
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
         <script src=" {{asset('js/index.js')}}"></script>
     </body>
